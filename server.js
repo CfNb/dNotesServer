785a1565c1888fc3d517e855cae4ffde34798921
DB Admin <dbadmin@DBs-iMac.local>
2016-12-27 09:15:34 -0600

    node service ok

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use('', function (req, res) {
-
+	console.log("OK");
+	res.send("OK");
 })
 
 app.listen(8080, function () {
-  console.log('app listening on port 8080!')
+  console.log('printflowmonitor app listening on port 8080!')
 })
